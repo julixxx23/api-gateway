4d10a89ba8720a74e78754146d5db1d2c006db36
julixxx23 <jorellanao12@miumg.edu.gt>
2026-05-01 22:22:23 -0600

    feat: coments

--- src/main/java/com/microservices/api/gateway/config/GatewayConfig.java
+++ src/main/java/com/microservices/api/gateway/config/GatewayConfig.java
@@ -1,5 +1,6 @@
 package com.microservices.api.gateway.config;
 
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.filter.LoadBalancerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
@@ -10,9 +11,12 @@ import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerRequest;
 import org.springframework.web.servlet.function.ServerResponse;
 
-@Configuration
-public class GatewayConfig {
+import java.net.URI;
 
+@Configuration
+public class GatewayConfig{
+
+    //Funcion de limpia registros y monitoreo del servicio Users
     private RouterFunction<ServerResponse> buildRoute(
             String routeId, String path, String serviceName) {
 
@@ -20,6 +24,10 @@ public class GatewayConfig {
                 .route(GatewayRequestPredicates.path(path),
                         HandlerFunctions.http())
                 .filter(LoadBalancerFilterFunctions.lb(serviceName))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker(
+                        serviceName + "-cb",
+                        URI.create("forward:/fallback/" + serviceName)
+                ))
                 .filter((request, next) -> {
                     String username = (String) request.servletRequest()
                             .getAttribute("X-User-Name");
@@ -35,7 +43,6 @@ public class GatewayConfig {
                 })
                 .build();
     }
-
     @Bean
     public RouterFunction<ServerResponse> userServiceRoute() {
         return buildRoute("user-service", "/api/v1/users/**", "user-service");
